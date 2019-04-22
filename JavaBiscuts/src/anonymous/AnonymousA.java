58d610d4c2f1a48e92f96e3ad8d06c2ac8cb44d5
PavithraTYSS26 <48939062+PavithraTYSS26@users.noreply.github.com>
2019-04-22 12:36:27 +0530

    i have inserted one new read method in anounymousa

--- JavaBiscuts/src/anonymous/AnonymousA.java
+++ JavaBiscuts/src/anonymous/AnonymousA.java
@@ -2,5 +2,8 @@ package anonymous;
 
 public abstract class AnonymousA {
 	public abstract void write();
+	public static void read(){
+	System.out.println("Hai Pavi I M modifying the class in git dont mind it ok.........?");
+	}
 
 }
